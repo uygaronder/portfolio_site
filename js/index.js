2c602de1cd6801adee6eb27223a4df974b690648
under4 <uonder50@gmail.com>
2022-07-26 23:27:39 +0300

    three stuff

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setClearColor("#050e1b");
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.getElementById("hero").appendChild(renderer.domElement);
+
+window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectMatrix();
+});
+
+renderer.render(scene, camera);
