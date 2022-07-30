4a2bc8524822357a9b82f83a8dc3bfc15572a6b9
under4 <uonder50@gmail.com>
2022-07-30 23:13:01 +0300

    stuff

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+const gui = new dat.GUI();
+const world = {
+    plane: {
+        width: 10,
+    },
+};
+
+gui.add(world.plane, "width", 1, 20).onChange(() => {
+    planeMesh.geometry.dispose();
+});
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -20,8 +31,17 @@ window.addEventListener("resize", () => {
 });
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
-const planeMaterial = new THREE.MeshPhongMaterial({ color: 0x05e1b });
+const planeMaterial = new THREE.MeshPhongMaterial({
+    color: 0x05e1b,
+    flatShading: THREE.FlatShading,
+});
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+
+const planeArray = planeMesh.geometry.vertices;
+for (const vertice of planeArray) {
+    const num = Math.floor(Math.random() * 10) * 0.03;
+    vertice.z += num;
+}
 
 scene.add(planeMesh);
 const light = new THREE.DirectionalLight(0xffffff, 1, 500);
